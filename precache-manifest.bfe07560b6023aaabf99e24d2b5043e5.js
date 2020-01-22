45edc987edf871cd3a07a07970f5eb6206496630
tomsza26 <tomsza27@gmail.com>
2020-01-22 18:39:14 +0100

    Updates

---
+++ precache-manifest.bfe07560b6023aaabf99e24d2b5043e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c592b535eb9df58e0253adf5cab3fe6",
+    "url": "/fcc-Calculator/index.html"
+  },
+  {
+    "revision": "b74f30e04afdecd22476",
+    "url": "/fcc-Calculator/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "b674ca14f8fe3135f5ab",
+    "url": "/fcc-Calculator/static/css/main.3612321b.chunk.css"
+  },
+  {
+    "revision": "b74f30e04afdecd22476",
+    "url": "/fcc-Calculator/static/js/2.7307ab49.chunk.js"
+  },
+  {
+    "revision": "a7fc8f0d37ddb08934cc9a90ac7fd961",
+    "url": "/fcc-Calculator/static/js/2.7307ab49.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b674ca14f8fe3135f5ab",
+    "url": "/fcc-Calculator/static/js/main.4d9c5302.chunk.js"
+  },
+  {
+    "revision": "79adbb2cbfa13056f3d6",
+    "url": "/fcc-Calculator/static/js/runtime-main.55cb4d6f.js"
+  }
+]);
